775b769c2c093755ab3d4f995aeac6e0c22cab50
Karla <ceciliaonofre23@gmail.com>
2017-12-08 12:02:04 -0600

    cambios

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,54 +1,7 @@
 package sample;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-
-public class Main extends Application {
-
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
-    }
+public class Main {
 
 
-    public static void main(String[] args) {
-        //launch(args);
-        Árbol arbol = new Árbol();
-        Nodo raiz = new Nodo(8);
-        Nodo nodo2 = new Nodo(7);
-        Nodo nodo3 = new Nodo(9);
-
-        nodo2.setNodoDerecho(new Nodo(3));
-        nodo2.setNodoIzquierdo(new Nodo(5));
-
-        nodo3.setNodoDerecho(new Nodo(4));
-        nodo3.setNodoDerecho(new Nodo(2));
-
-        raiz.setNodoIzquierdo(nodo2);
-        raiz.setNodoDerecho(nodo3);
-
-        System.out.println("PreOrden");
-        arbol.preOrden(raiz);
-        System.out.print("|");
-        System.out.println(" ");
-        System.out.println("-------------------------");
-        System.out.println("InOrden: ");
-        arbol.InOrden(raiz);
-        System.out.print("|");
-        System.out.println(" ");
-        System.out.println("-------------------------");
-        System.out.println("PostOrden: ");
-        arbol.PostOrden(raiz);
-        System.out.print("|");
-        System.out.println("-------------------------");
-
-    }
-        
-    }
+}
 
--- src/sample/Nodo.java
+++ src/sample/Nodo.java
@@ -4,49 +4,6 @@ package sample;
  * Created by Akado on 29/11/2017.
  */
 public class Nodo {
-<<<<<<< HEAD
-    private Nodo raiz, der, izq;
-    public int zise;
 
-=======
-    private int raiz;
-    private Nodo der, izq;
->>>>>>> origin/uriel
 
-    public  Object valor;
-    public Nodo(Object valor) {
-        this.valor = valor;
-    }
-<<<<<<< HEAD
-    public int size getzise(){
-        return zise;
-    }
-
-    public Nodo getRaiz(){
-        return raiz;
-=======
-    public Nodo(int raiz){
-        this.raiz = raiz;
->>>>>>> origin/uriel
-    }
-
-    public Nodo getDer(){
-        return der;
-    }
-
-    public Nodo getIzq(){
-        return izq;
-    }
-
-    public void setNodoDerecho(Nodo nodo){
-        der = nodo;
-    }
-    public void setNodoIzquierdo(Nodo nodo){
-        izq = nodo;
-    }
-
-    //Para obtener el dato
-    public int getRaiz(){
-        return raiz;
-    }
 }
